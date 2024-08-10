7f443d19101f0e9d75c26a33f1b6f8cbb003d09d
kdg391 <105781186+kdg391@users.noreply.github.com>
2024-08-10 10:07:52 +0900

    fix: unused variable

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,8 +1,4 @@
-import { createTranslation } from '../../i18n/server.js'
-
 const NotFound = async () => {
-  const { t } = await createTranslation('translation')
-
   return (
     <div>
       <h1>Map Not Found</h1>
